6db1501e4737bce27d9d17c278a9ed806e63b8bb
pavel <pavelkukuskmn388@gmail.com>
2021-02-13 17:38:41 +0300

    create react app, added config firebase in firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXgxGf9MV8ILHIFalb79adohAole65ebE",
+    authDomain: "clone-75433.firebaseapp.com",
+    projectId: "clone-75433",
+    storageBucket: "clone-75433.appspot.com",
+    messagingSenderId: "942001103463",
+    appId: "1:942001103463:web:2938648e001dc0ffe82ac2",
+    measurementId: "G-3JHLK9XD9T"
+};
